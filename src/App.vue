e588400d69a1c5e6dd9df8f263cf57deb9279336
teller2016 <teller2016@ajou.ac.kr>
2022-12-04 22:35:06 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="name">
+    {{greeting(name)}}
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = 'Molly Coding';
+
+    const greeting = (name) => {
+      return `Hello ${name}`;
+    };
+
+    return {
+      name,
+      greeting
+    }
+  }
+}
+</script>
+
+<style>
+  .name {
+    color: red;
+  }
+</style>
