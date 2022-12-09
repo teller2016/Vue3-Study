e2c7fadf20a4770a196851aa5f472c411a3686ec
teller2016 <teller2016@ajou.ac.kr>
2022-12-09 22:58:47 +0900

    props, emit, computed study + search filter added

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,90 @@
 <template>
-  <div class="name">
-    {{name}}
+  <div class="container">
+    <h2>To-Do List</h2>
+
+    <input 
+      class="form-control"
+      type="text" 
+      v-model="searchText"
+      placeholder="Search"
+    >
+    <hr/>
+    <TodoSimpleForm @add-todo="addTodo"/>
+
+    <div v-if="!todos.length">
+      추가된 ToDo가 없습니다
+    </div>
+
+    <div v-if="!filteredTodos.length">
+      There is nothing to display
+    </div>
+    <TodoList 
+      :todos="filteredTodos" 
+      @toggle-todo="toggleTodo" 
+      @delete-todo="deleteTodo"/>
+    
   </div>
-  <button 
-    class="btn btn-primary"
-    v-on:click="updateName"
-    >click</button>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref, computed } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
-  setup() {
-    const name = reactive({
-      id: 'molly',
-    })
+  components: {
+    TodoSimpleForm,
+    TodoList,
+  },
 
-    const updateName = () => {
-      name.id = 'Molly';
+  setup() {
+    const todos = ref([
+      {
+        id: 1,
+        subject: '턱걸이 하기',
+        completed: false,
+      }
+    ]);
+
+    const addTodo = (todo) => {
+      todos.value.push(todo);
     }
 
+    const toggleTodo = (index) => {
+      todos.value[index].completed = !todos.value[index].completed;
+    };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    const searchText = ref('');
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todos.value;
+    });
+
     return {
-      name,
-      updateName
+      todos,
+      addTodo,
+      toggleTodo,
+      deleteTodo,
+      searchText,
+      filteredTodos
     }
   }
 }
 </script>
 
 <style>
-  .name {
-    color: red;
+  .todo {
+    color: gray;
+    text-decoration: line-through;
   }
+
 </style>
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,63 @@
+<template>
+    <form 
+      @submit.prevent="onSubmit"
+    >
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input 
+            class="form-control"
+            type="text" 
+            v-model="todo"
+            placeholder="Type new to-do"
+          >
+        </div>
+  
+        <div>
+          <button 
+            class="btn btn-primary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+
+      <p v-show="hasError" style="color: red;">Error</p>
+    </form>
+</template>
+
+<script>
+    import { ref } from 'vue';
+
+    export default {
+        emits: ['add-todo'],
+        setup(props, { emit }) {
+            const todo = ref('');
+            const hasError = ref(false);
+
+            const onSubmit = () => {
+                if (!todo.value) {
+                    hasError.value = true;
+                    return;
+                }
+
+                emit('add-todo', {
+                    id: Date.now(),
+                    subject: todo.value,
+                    completed: false,
+                });
+
+                hasError.value = false;
+                todo.value = '';
+            }
+
+            return {
+                todo,
+                hasError,
+                onSubmit
+            }
+        }
+    }
+</script>
+
+<style></style>
