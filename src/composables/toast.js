3bf6ff8e864d1a3945ad67153e55bd5644651de6
teller2016 <teller2016@ajou.ac.kr>
2023-01-17 23:14:40 +0900

    vuex mutations, actions use to toast functions

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -1,29 +1,16 @@
-import { onUnmounted, computed } from 'vue';
+import { computed } from 'vue';
 import { useStore } from 'vuex';
 
 export const useToast = () => {
     const store = useStore();
 
-    const toastMessage = computed(() => store.state.toastMessage);
+    const toastMessage = computed(() => store.getters.toastMessageWithSmile);
     const toastAlertType = computed(() => store.state.toastAlertType);
     const showToast = computed(() => store.state.showToast);
-    const timeout = computed(() => store.state.timeout);
 
     const triggerToast = (message, type) => {
-        showToast.value = true;
-        toastMessage.value = message;
-        toastAlertType.value = type;
-
-        timeout.value = setTimeout(() => {
-            toastMessage.value = '';
-            showToast.value = false;
-            toastAlertType.value = '';
-        }, 3000);
+        store.dispatch('triggerToast', message, type);
     }
-
-    onUnmounted(() => {
-        clearTimeout(timeout.value);
-    });
 
     return {
         toastMessage,
