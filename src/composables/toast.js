a00b0246fe3dcb6fcb75abe10345b88fdb7726ab
teller2016 <teller2016@ajou.ac.kr>
2023-01-19 22:19:24 +0900

    multiple toast

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -3,19 +3,20 @@ import { useStore } from 'vuex';
 
 export const useToast = () => {
     const store = useStore();
-
-    const toastMessage = computed(() => store.getters['toast/toastMessageWithSmile']);
-    const toastAlertType = computed(() => store.state.toast.toastAlertType);
-    const showToast = computed(() => store.state.toast.showToast);
+    // const toastMessage = computed(() => store.getters['toast/toastMessageWithSmile']);
+    // const toastAlertType = computed(() => store.state.toast.toastAlertType);
+    // const showToast = computed(() => store.state.toast.showToast);
+    const toasts = computed(() => store.state.toast.toasts);
 
     const triggerToast = (message, type) => {
         store.dispatch('toast/triggerToast', message, type);
     }
 
     return {
-        toastMessage,
-        toastAlertType,
-        showToast,
+        // toastMessage,
+        // toastAlertType,
+        // showToast,
+        toasts,
         triggerToast,
     }
 };
--- src/store/modules/toast/index.js
+++ src/store/modules/toast/index.js
@@ -1,33 +1,44 @@
 export default {
     namespaced: true,
     state: {
-        toastMessage: '',
-        toastAlertType: '',
-        showToast: false,
-        timeout: null,
+        toasts: [],
+        // toastMessage: '',
+        // toastAlertType: '',
+        // showToast: false,
     },
     mutations: {
-        UPDATE_TOAST_MESSAGE (state, payload) {
-            state.toastMessage = payload;
+        // UPDATE_TOAST_MESSAGE (state, payload) {
+        //     state.toastMessage = payload;
+        // },
+        // UPDATE_TOAST_ALERT_TYPE (state, payload) {
+        //     state.toastAlertType = payload;
+        // },
+        // UPDATE_TOAST_STATUS (state, payload) {
+        //     state.showToast = payload;
+        // },
+        ADD_TOAST (state, payload) {
+            state.toasts.push(payload);
         },
-        UPDATE_TOAST_ALERT_TYPE (state, payload) {
-            state.toastAlertType = payload;
-        },
-        UPDATE_TOAST_STATUS (state, payload) {
-            state.showToast = payload;
+        REMOVE_TOAST (state) {
+            state.toasts.shift();
         },
     },
     actions: {
         triggerToast({commit}, message, type = 'success') {
-            commit('UPDATE_TOAST_MESSAGE', message);
-            commit('UPDATE_TOAST_ALERT_TYPE', type);
-            commit('UPDATE_TOAST_STATUS', true);
+            // commit('UPDATE_TOAST_MESSAGE', message);
+            // commit('UPDATE_TOAST_ALERT_TYPE', type);
+            // commit('UPDATE_TOAST_STATUS', true);
+            commit('ADD_TOAST', {
+                id: Date.now(),
+                message,
+                type,
+            });
 
             setTimeout(() => {
-                commit('UPDATE_TOAST_MESSAGE', '');
-                commit('UPDATE_TOAST_ALERT_TYPE', '');
-                commit('UPDATE_TOAST_STATUS', false);
-
+                // commit('UPDATE_TOAST_MESSAGE', '');
+                // commit('UPDATE_TOAST_ALERT_TYPE', '');
+                // commit('UPDATE_TOAST_STATUS', false);
+                commit('REMOVE_TOAST');
             }, 5000);
         }
     },
